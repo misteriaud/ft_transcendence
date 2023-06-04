994ceb52541b4d59851e96e3f2bd7158771acb71
spayeur <spayeur@student.42.fr>
2023-06-04 19:13:30 +0200

    <fix> MenuRoomItems and UserUI forwardRef

--- apps/react/src/components/user.tsx
+++ apps/react/src/components/user.tsx
@@ -195,194 +195,196 @@ export function MuteDialog({ user, room, mutateRoom, dialogStatus, dialogHandler
 	);
 }
 
-const MenuRoomItems = forwardRef(
-	(
-		{ me, user, room_id, menuHandler: handleOpenCloseMenu, ...otherProps }: { me: i_me; user: i_user; room_id: number; menuHandler: any; otherProps?: any },
-		ref: any
-	) => {
-		const {
-			room,
-			mutate: mutateRoom,
-			isLoading: isLoadingRoom,
-			error: errorRoom
-		}: { isLoading: boolean; room: i_room; mutate: KeyedMutator<i_room>; error: Error } = useRoom(room_id);
-		const [openMuteDialog, setOpenMuteDialog] = useState(false);
-		const api = useApi();
+const MenuRoomItems = forwardRef((props: any, ref: any) => {
+	const {
+		me,
+		user,
+		room_id,
+		menuHandler: handleOpenCloseMenu,
+		...otherProps
+	}: { me: i_me; user: i_user; room_id: number; menuHandler: any; otherProps?: any } = props;
+	const {
+		room,
+		mutate: mutateRoom,
+		isLoading: isLoadingRoom,
+		error: errorRoom
+	}: { isLoading: boolean; room: i_room; mutate: KeyedMutator<i_room>; error: Error } = useRoom(room_id);
+	const [openMuteDialog, setOpenMuteDialog] = useState(false);
+	const api = useApi();
 
-		if (isLoadingRoom) {
-			return (
-				<>
-					<hr ref={ref} {...otherProps} className="my-2 border-blue-gray-50 bg-white outline-none" />
-					<div ref={ref} {...otherProps} className="flex justify-around p-2 outline-none">
-						<Spinner />
-					</div>
-				</>
-			);
-		}
-		if (errorRoom) {
-			// error
-			return (
-				<>
-					<hr ref={ref} {...otherProps} className="my-2 border-blue-gray-50 outline-none" />
-					<div
-						ref={ref}
-						{...otherProps}
-						className="flex justify-around p-2 rounded-md text-red-500 outline-none hover:text-red-900 bg-white hover:!bg-red-50 hover:bg-opacity-80"
-						onClick={() => mutateRoom()}
-					>
-						<ExclamationTriangleIcon strokeWidth={2} className="h-6 w-6 text-red-500" />
-					</div>
-				</>
-			);
-		}
-
-		const isMeOwnerOfRoom = room.members.some((m: i_member) => m.user.id === me.id && m.role === e_member_role.OWNER);
-		const isMeAdminOfRoom = room.members.some((m: i_member) => m.user.id === me.id && m.role === e_member_role.ADMIN);
-		const isUserOwnerOfRoom = room.members.some((m: i_member) => m.user.id === user.id && m.role === e_member_role.OWNER);
-		const isUserAdminOfRoom = room.members.some((m: i_member) => m.user.id === user.id && m.role === e_member_role.ADMIN);
-		const isUserMuted = room.members.some((m: i_member) => m.user.id === user.id && m.muted === true);
-		const isUserBanned = room.members.some((m: i_member) => m.user.id === user.id && m.banned === true);
-
-		const hidePromote = !isMeOwnerOfRoom || isUserAdminOfRoom || isUserBanned || isUserOwnerOfRoom;
-		const hideDemote = !isMeOwnerOfRoom || !isUserAdminOfRoom || isUserBanned;
-		const hideMute = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || isUserMuted || isUserBanned || isUserOwnerOfRoom;
-		const hideUnmute = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || !isUserMuted || isUserBanned || isUserOwnerOfRoom;
-		const hideKick = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || isUserBanned || isUserOwnerOfRoom;
-		const hideBan = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || isUserBanned || isUserOwnerOfRoom;
-		const hideUnban = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || !isUserBanned || isUserOwnerOfRoom;
-
-		async function handlePromote() {
-			handleOpenCloseMenu();
-			await api
-				.put(`/rooms/${room_id}/promote/${user.id}`)
-				.then(() => {
-					mutateRoom();
-				})
-				.catch(() => {
-					// error
-				});
-		}
-
-		async function handleDemote() {
-			handleOpenCloseMenu();
-			await api
-				.put(`/rooms/${room_id}/demote/${user.id}`)
-				.then(() => {
-					mutateRoom();
-				})
-				.catch(() => {
-					// error
-				});
-		}
-
-		function handleMute() {
-			if (openMuteDialog) {
-				handleOpenCloseMenu();
-			}
-			setOpenMuteDialog(!openMuteDialog);
-		}
-
-		async function handleUnmute() {
-			handleOpenCloseMenu();
-			await api
-				.put(`/rooms/${room_id}/unmute/${user.id}`)
-				.then(() => {
-					mutateRoom();
-				})
-				.catch(() => {
-					// error
-				});
-		}
-
-		async function handleKick() {
-			handleOpenCloseMenu();
-			await api
-				.delete(`/rooms/${room_id}/kick/${user.id}`)
-				.then(() => {
-					mutateRoom();
-				})
-				.catch(() => {
-					// error
-				});
-		}
-
-		async function handleBan() {
-			handleOpenCloseMenu();
-			await api
-				.put(`/rooms/${room_id}/ban/${user.id}`)
-				.then(() => {
-					mutateRoom();
-				})
-				.catch(() => {
-					// error
-				});
-		}
-
-		async function handleUnban() {
-			handleOpenCloseMenu();
-			await api
-				.put(`/rooms/${room_id}/unban/${user.id}`)
-				.then(() => {
-					mutateRoom();
-				})
-				.catch(() => {
-					// error
-				});
-		}
-
-		if (hidePromote && hideDemote && hideMute && hideUnmute && hideKick && hideBan && hideUnban) {
-			return <></>;
-		}
+	if (isLoadingRoom) {
 		return (
 			<>
-				<hr ref={ref} {...otherProps} className="my-2 border-blue-gray-50 outline-none" />
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hidePromote && 'hidden'}`} onClick={handlePromote} tabIndex={-1}>
-					<SparklesIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Promote
-					</Typography>
-				</MenuItem>
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideDemote && 'hidden'}`} onClick={handleDemote} tabIndex={-1}>
-					<SparklesIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Demote
-					</Typography>
-				</MenuItem>
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideMute && 'hidden'}`} onClick={handleMute} tabIndex={-1}>
-					<SpeakerXMarkIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Mute
-					</Typography>
-				</MenuItem>
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideUnmute && 'hidden'}`} onClick={handleUnmute} tabIndex={-1}>
-					<SpeakerWaveIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Unmute
-					</Typography>
-				</MenuItem>
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideKick && 'hidden'}`} onClick={handleKick} tabIndex={-1}>
-					<ArrowLeftOnRectangleIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Kick
-					</Typography>
-				</MenuItem>
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideBan && 'hidden'}`} onClick={handleBan} tabIndex={-1}>
-					<ScaleIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Ban
-					</Typography>
-				</MenuItem>
-				<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideUnban && 'hidden'}`} onClick={handleUnban} tabIndex={-1}>
-					<ScaleIcon strokeWidth={2} className="h-4 w-4" />
-					<Typography variant="small" className="font-normal">
-						Unban
-					</Typography>
-				</MenuItem>
-				<MuteDialog user={user} room={room} mutateRoom={mutateRoom} dialogStatus={openMuteDialog} dialogHandler={handleMute} />
+				<hr ref={ref} {...otherProps} className="my-2 border-blue-gray-50 bg-white outline-none" />
+				<div ref={ref} {...otherProps} className="flex justify-around p-2 outline-none">
+					<Spinner />
+				</div>
 			</>
 		);
 	}
-);
+	if (errorRoom) {
+		// error
+		return (
+			<>
+				<hr ref={ref} {...otherProps} className="my-2 border-blue-gray-50 outline-none" />
+				<div
+					ref={ref}
+					{...otherProps}
+					className="flex justify-around p-2 rounded-md text-red-500 outline-none hover:text-red-900 bg-white hover:!bg-red-50 hover:bg-opacity-80"
+					onClick={() => mutateRoom()}
+				>
+					<ExclamationTriangleIcon strokeWidth={2} className="h-6 w-6 text-red-500" />
+				</div>
+			</>
+		);
+	}
+
+	const isMeOwnerOfRoom = room.members.some((m: i_member) => m.user.id === me.id && m.role === e_member_role.OWNER);
+	const isMeAdminOfRoom = room.members.some((m: i_member) => m.user.id === me.id && m.role === e_member_role.ADMIN);
+	const isUserOwnerOfRoom = room.members.some((m: i_member) => m.user.id === user.id && m.role === e_member_role.OWNER);
+	const isUserAdminOfRoom = room.members.some((m: i_member) => m.user.id === user.id && m.role === e_member_role.ADMIN);
+	const isUserMuted = room.members.some((m: i_member) => m.user.id === user.id && m.muted === true);
+	const isUserBanned = room.members.some((m: i_member) => m.user.id === user.id && m.banned === true);
+
+	const hidePromote = !isMeOwnerOfRoom || isUserAdminOfRoom || isUserBanned || isUserOwnerOfRoom;
+	const hideDemote = !isMeOwnerOfRoom || !isUserAdminOfRoom || isUserBanned;
+	const hideMute = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || isUserMuted || isUserBanned || isUserOwnerOfRoom;
+	const hideUnmute = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || !isUserMuted || isUserBanned || isUserOwnerOfRoom;
+	const hideKick = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || isUserBanned || isUserOwnerOfRoom;
+	const hideBan = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || isUserBanned || isUserOwnerOfRoom;
+	const hideUnban = (!isMeOwnerOfRoom && !isMeAdminOfRoom) || !isUserBanned || isUserOwnerOfRoom;
+
+	async function handlePromote() {
+		handleOpenCloseMenu();
+		await api
+			.put(`/rooms/${room_id}/promote/${user.id}`)
+			.then(() => {
+				mutateRoom();
+			})
+			.catch(() => {
+				// error
+			});
+	}
+
+	async function handleDemote() {
+		handleOpenCloseMenu();
+		await api
+			.put(`/rooms/${room_id}/demote/${user.id}`)
+			.then(() => {
+				mutateRoom();
+			})
+			.catch(() => {
+				// error
+			});
+	}
+
+	function handleMute() {
+		if (openMuteDialog) {
+			handleOpenCloseMenu();
+		}
+		setOpenMuteDialog(!openMuteDialog);
+	}
+
+	async function handleUnmute() {
+		handleOpenCloseMenu();
+		await api
+			.put(`/rooms/${room_id}/unmute/${user.id}`)
+			.then(() => {
+				mutateRoom();
+			})
+			.catch(() => {
+				// error
+			});
+	}
+
+	async function handleKick() {
+		handleOpenCloseMenu();
+		await api
+			.delete(`/rooms/${room_id}/kick/${user.id}`)
+			.then(() => {
+				mutateRoom();
+			})
+			.catch(() => {
+				// error
+			});
+	}
+
+	async function handleBan() {
+		handleOpenCloseMenu();
+		await api
+			.put(`/rooms/${room_id}/ban/${user.id}`)
+			.then(() => {
+				mutateRoom();
+			})
+			.catch(() => {
+				// error
+			});
+	}
+
+	async function handleUnban() {
+		handleOpenCloseMenu();
+		await api
+			.put(`/rooms/${room_id}/unban/${user.id}`)
+			.then(() => {
+				mutateRoom();
+			})
+			.catch(() => {
+				// error
+			});
+	}
+
+	if (hidePromote && hideDemote && hideMute && hideUnmute && hideKick && hideBan && hideUnban) {
+		return <></>;
+	}
+	return (
+		<>
+			<hr ref={ref} {...otherProps} className="my-2 border-blue-gray-50 outline-none" />
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hidePromote && 'hidden'}`} onClick={handlePromote} tabIndex={-1}>
+				<SparklesIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Promote
+				</Typography>
+			</MenuItem>
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideDemote && 'hidden'}`} onClick={handleDemote} tabIndex={-1}>
+				<SparklesIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Demote
+				</Typography>
+			</MenuItem>
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideMute && 'hidden'}`} onClick={handleMute} tabIndex={-1}>
+				<SpeakerXMarkIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Mute
+				</Typography>
+			</MenuItem>
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideUnmute && 'hidden'}`} onClick={handleUnmute} tabIndex={-1}>
+				<SpeakerWaveIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Unmute
+				</Typography>
+			</MenuItem>
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideKick && 'hidden'}`} onClick={handleKick} tabIndex={-1}>
+				<ArrowLeftOnRectangleIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Kick
+				</Typography>
+			</MenuItem>
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideBan && 'hidden'}`} onClick={handleBan} tabIndex={-1}>
+				<ScaleIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Ban
+				</Typography>
+			</MenuItem>
+			<MenuItem ref={ref} {...otherProps} className={`flex items-center gap-2 outline-none ${hideUnban && 'hidden'}`} onClick={handleUnban} tabIndex={-1}>
+				<ScaleIcon strokeWidth={2} className="h-4 w-4" />
+				<Typography variant="small" className="font-normal">
+					Unban
+				</Typography>
+			</MenuItem>
+			<MuteDialog user={user} room={room} mutateRoom={mutateRoom} dialogStatus={openMuteDialog} dialogHandler={handleMute} />
+		</>
+	);
+});
 
 export function User({ room_id, login42 }: { room_id?: number; login42: string }) {
 	const { me, mutate: mutateMe, isLoading: isLoadingMe, error: errorMe }: { isLoading: boolean; me: i_me; mutate: KeyedMutator<i_me>; error: Error } = useMe();
--- apps/react/src/components/userUI.tsx
+++ apps/react/src/components/userUI.tsx
@@ -1,20 +1,14 @@
 import { Avatar, Badge, Typography } from '@material-tailwind/react';
 import { forwardRef } from 'react';
 
-export const UserUI = forwardRef(function UserUI(
-	{
-		username,
-		avatar,
-		inverse,
-		onClick: onClickHandler
-	}: { username: string; avatar: string | undefined; inverse?: boolean; onClick?: React.MouseEventHandler<HTMLDivElement> },
-	ref: any
-) {
+export const UserUI = forwardRef((props: any, ref: any) => {
+	const { username, avatar, inverse, ...otherProps }: { username: string; avatar: string | undefined; inverse?: boolean; otherProps: any } = props;
+
 	return (
 		<div
 			ref={ref}
 			className="flex justify-center items-center p-3 rounded-md text-blue-gray-500 hover:text-blue-gray-900 bg-white hover:bg-blue-gray-50 hover:bg-opacity-80"
-			onClick={onClickHandler}
+			{...otherProps}
 		>
 			{inverse && (
 				<Typography variant="h5" className="font-normal mr-3">
