57846c293c813cc02dce81004cf0f3a50f377dc2
jb <jvermeer@student.42.fr>
2023-06-09 16:57:24 +0200

    Carousel Profile Page with Progress bar for win rate

--- apps/react/src/pages/Profile.tsx
+++ apps/react/src/pages/Profile.tsx
@@ -1,82 +1,98 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Carousel, Spinner, Typography } from '@material-tailwind/react';
+import { Carousel, Spinner, Typography, Progress } from '@material-tailwind/react';
 import { useMe, useUser } from '../hooks/useUser';
 import { User } from '../components/user';
-import defaultImg from '../images/pingpong-map.jpg';
-import carouselBg from '../images/backgroundHomePage.jpg';
+import { i_user } from '../components/interfaces';
 import { useState } from 'react';
 
-function CarouselUserData() {
+function CarouselUserData({ user }: { user: i_user }) {
+	const formattedDate = new Date(user.createdAt).toLocaleDateString('fr-FR', { year: 'numeric', month: 'numeric', day: 'numeric' });
 	return (
-		<>
-			<img src={carouselBg} alt="image 1" className="h-full w-full object-cover" />
-			<div className="absolute inset-0 grid h-full w-full place-items-center">
-				{/* <div className="bg-gray-300 absolute inset-0 grid h-full w-full place-items-center"> */}
-				<div>
-					<div className="flex gap-8">
-						<div className="rounded-full h-48 w-48 border-4 border-gray-800">
-							<img src={defaultImg} className="rounded-full h-full w-full object-cover" alt="user-img" />
-						</div>
-						<div className="flex flex-col items-center justify-center w-60">
-							<Typography variant="h4" color="blue-gray" className="mt-8 truncate max-w-full">
-								Unlongblazzzeee
-							</Typography>
-							<Typography color="blue-gray" className="truncate max-w-full font-normal font-mono text-2xl">
-								jvermeer
-							</Typography>
-							<Typography color="gray" className="mt-4 italic truncate max-w-full font-normal font-mono text-xl">
-								member since:
-							</Typography>
-							<Typography color="gray" className=" italic truncate max-w-full font-normal font-mono text-lg">
-								08/06/2023
-							</Typography>
-						</div>
+		// <div className="bg-gradient-to-r from-deep-orange-200 to-amber-100 absolute inset-0 grid h-full w-full place-items-center">
+		<div className="bg-gradient-to-r from-blue-gray-100 to-blue-gray-50 absolute inset-0 grid h-full w-full place-items-center">
+			<div>
+				<div className="flex gap-8">
+					<div className="rounded-full bg-gray-800 h-48 w-48 border-4 border-gray-800">
+						<img src={user.avatar} className="rounded-full h-full w-full object-cover" alt="user-img" />
+					</div>
+					<div className="flex flex-col items-center justify-center w-60">
+						<Typography variant="h4" color="blue-gray" className="mt-8 truncate max-w-full">
+							{user.username}
+						</Typography>
+						<Typography color="blue-gray" className="truncate max-w-full font-normal font-mono text-2xl">
+							{user.login42}
+						</Typography>
+						<Typography color="gray" className="mt-4 italic truncate max-w-full font-normal font-mono text-xl">
+							member since:
+						</Typography>
+						<Typography color="gray" className=" italic truncate max-w-full font-normal font-mono text-lg">
+							{formattedDate}
+						</Typography>
 					</div>
 				</div>
-			</div>
-		</>
-	);
-}
-
-function CarouselStats() {
-	const [wins, setWins] = useState(5);
-	const [loses, setLoses] = useState(19);
-	const winrate = (wins / loses).toFixed(2);
-	return (
-		<div className="h-full w-full bg-cover bg-center p-16 flex justify-center items-center" style={{ backgroundImage: `url(${carouselBg})` }}>
-			<div className="flex-1 flex flex-col justify-center items-center">
-				<Typography variant="h2" color="green">
-					Wins
-				</Typography>
-				<Typography variant="h1" color="black">
-					{wins}
-				</Typography>
-			</div>
-			<div className="flex-1 flex flex-col justify-center items-center">
-				<Typography variant="h4" color="gray">
-					win rate:
-				</Typography>
-				<Typography variant="h4" color="blue-gray">
-					{winrate}
-				</Typography>
-			</div>
-			<div className="flex-1 flex flex-col justify-center items-center">
-				<Typography variant="h2" color="red">
-					Loses
-				</Typography>
-				<Typography variant="h1" color="black">
-					{loses}
-				</Typography>
 			</div>
 		</div>
 	);
 }
 
-function ProfileCarousel() {
+function ProgressBar({ winrate }: { winrate: number }) {
 	return (
-		<div className="h-screen p-4 mb-30">
+		<div className="w-full p-4 flex-col items-center justify-center mt-6">
+			<Typography color="gray" variant="h6" className="text-center">
+				win rate
+			</Typography>
+			{winrate >= 50 ? (
+				<>
+					<Progress value={winrate} variant="gradient" size="md" color="teal" />
+					<Typography color="teal" variant="h6" className="text-center">
+						{winrate}%
+					</Typography>
+				</>
+			) : (
+				<>
+					<Progress value={winrate} variant="gradient" size="md" color="red" />
+					<Typography color="red" variant="h6" className="text-center">
+						{winrate}%
+					</Typography>
+				</>
+			)}
+		</div>
+	);
+}
+
+function CarouselStats({ user }: { user: i_user }) {
+	const [wins, setWins] = useState(24);
+	const [loses, setLoses] = useState(8);
+	// const winrate = wins !== 0 || loses !== 0 ? ((wins * 100) / (wins + loses)).toFixed(1) : null;
+	const winrate: number = wins !== 0 || loses !== 0 ? parseFloat(((wins * 100) / (wins + loses)).toFixed(0)) : 0;
+	return (
+		// <div className="h-full w-full bg-gradient-to-l from-deep-orange-200 to-amber-100 p-16 flex justify-center items-center">
+		<div className="h-full w-full bg-gradient-to-l from-blue-gray-100 to-blue-gray-50 p-16 flex justify-center items-center">
+			<div className="flex-1 flex flex-col justify-center items-center">
+				<Typography color="teal" className="font-sans text-3xl font-bold">
+					WINS
+				</Typography>
+				<Typography className="font-sans text-4xl font-bold text-gray-700">{wins}</Typography>
+			</div>
+			<div className="flex-1 flex flex-col justify-center items-center">
+				<ProgressBar winrate={winrate} />
+			</div>
+			<div className="flex-1 flex flex-col justify-center items-center">
+				<Typography color="red" className="font-sans text-3xl font-bold tracking-tighter">
+					LOSES
+				</Typography>
+				<Typography className="font-sans text-4xl font-bold text-gray-700">{loses}</Typography>
+			</div>
+		</div>
+	);
+}
+
+function ProfileCarousel({ user }: { user: i_user }) {
+	return (
+		<div className="h-64 p-4">
 			<Carousel
-				className="h-64 mb-6 rounded-3xl"
+				className="h-full mb-6 rounded-3xl"
 				navigation={({ setActiveIndex, activeIndex, length }) => (
 					<div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
 						{new Array(length).fill('').map((_, i) => (
@@ -89,8 +105,8 @@ function ProfileCarousel() {
 					</div>
 				)}
 			>
-				<CarouselUserData />
-				<CarouselStats />
+				<CarouselUserData user={user} />
+				<CarouselStats user={user} />
 			</Carousel>
 		</div>
 	);
@@ -98,7 +114,7 @@ function ProfileCarousel() {
 
 export function ProfilePage() {
 	const { username } = useParams();
-	const { isLoading: isLoadingMe /*, me*/ } = useMe();
+	const { isLoading: isLoadingMe } = useMe();
 	const { isLoading: isLoadingUser, user } = useUser(username || '');
 
 	if (isLoadingMe || isLoadingUser) {
@@ -107,10 +123,11 @@ export function ProfilePage() {
 	if (!user) {
 		return <h2>User not Found</h2>;
 	}
+	console.log(user.username);
 	return (
 		<div className="h-screen bg-gray-200">
 			<div className="flex flex-col">
-				{/* <ProfileCarousel /> */}
+				<ProfileCarousel user={user} />
 				<div>
 					<User room_id={1} login42={user.login42}></User>
 				</div>
