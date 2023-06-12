567738551ea85db50251c4646fb133703bc4d224
DimiOui <omygoditsdimi@gmail.com>
2023-06-12 21:37:36 +0200

    <fix>Queue timer resetting to 0

--- apps/react/src/components/Queue.tsx
+++ apps/react/src/components/Queue.tsx
@@ -133,6 +133,7 @@ export function GameButton() {
 
 	const handleJoin = () => {
 		setLoading(true);
+		setTimer(0);
 		handleOpen();
 		socket.emit('pong/invite', invitation, (invitationId: string) => {
 			setInvitation({ ...invitation, id: invitationId });
